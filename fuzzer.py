21ce637d392f5040607bec174f160469c6429d0d
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-12-12 17:21:24 +0100

    apache2 license to be compatible with AFL

--- fuzzer.py
+++ fuzzer.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+"""
+
+   frida-fuzzer - fuzzer driver
+   ----------------------------
+
+   Written and maintained by Andrea Fioraldi <andreafioraldi@gmail.com>
+   Based on American Fuzzy Lop by Michal Zalewski
+
+   Copyright 2019 Andrea Fioraldi. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+"""
 
 import frida
 import base64
